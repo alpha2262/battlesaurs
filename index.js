7d2d2c491550b869d5c73346f09c72c599ff7580
Julia Schultz <julia.schultz91@gmail.com>
2016-07-28 11:38:48 -0400

    chat box added to html and css

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ var Message = mongoose.model("Message", new mongoose.Schema({
   text: String
 }));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('send:chatmessage', function(msg){
+    io.emit('posting:chatmessage', msg);
+  });
+});
 
 http.listen(3000, function () {
   console.log("This shit is live on 3000 yo.");
