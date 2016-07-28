cc77c9eef21552b3090bc9d582f9263a27aa9b62
Julia Schultz <julia.schultz91@gmail.com>
2016-07-28 14:58:39 -0400

    working on socket for div colors

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://localhost/battlesaurs");
-var Message = mongoose.model("Message", new mongoose.Schema({
-  text: String
-}));
+
+// mongoose.connect("mongodb://localhost/battlesaurs");
+// var Message = mongoose.model("Message", new mongoose.Schema({
+//   text: String
+// }));
 
 app.get("/", function (req, res) {
+  console.log("connected to index.html")
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log("a user has connected")
   socket.on('send:chatmessage', function(msg){
     io.emit('posting:chatmessage', msg);
   });
+  socket.on('send:litebrite', function(lite){
+    console.log(lite)
+    io.emit('posting:litebrite', lite);
+  })
 });
+
+
+
 
 
 http.listen(3300, function () {
